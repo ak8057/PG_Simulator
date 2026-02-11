10fc8bff279c7aac4fcf29f40c65dcc983642278
Abhay Kumar <abhay292005@gmail.com>
2026-02-11 17:55:25 +0530

    Initialize spring boot project

---
+++ src/main/java/com/payu/payu_sim/PayuSimApplication.java
@@ -0,0 +1,13 @@
+package com.payu.payu_sim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayuSimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayuSimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payu/payu_sim/PayuSimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payu.payu_sim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayuSimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
